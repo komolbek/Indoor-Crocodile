b1c8f1d0b00bbc183daa420ceb4fbd5e6f4b52c4
Kamol Ibragimov <k.ibragimov@softteco.com>
2021-12-06 19:44:57 +0000

    project upload q

---
+++ Indoor-Crocodile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Indoor-Crocodile
+//
+//  Created by Komolbek Ibragimov on 06/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Indoor-Crocodile/Indoor_CrocodileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indoor_CrocodileApp.swift
+//  Indoor-Crocodile
+//
+//  Created by Komolbek Ibragimov on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Indoor_CrocodileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
