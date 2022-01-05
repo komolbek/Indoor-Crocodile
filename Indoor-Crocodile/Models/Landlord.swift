7f7e3765181fb028c26d60824ee789f8e81b313b
Kamol Ibragimov <k.ibragimov@softteco.com>
2022-01-05 19:39:05 +0000

    created Landlord & Tenant model structs

---
+++ Indoor-Crocodile/Models/Landlord.swift
@@ -0,0 +1,13 @@
+//
+//  Landlord.swift
+//  Indoor-Crocodile
+//
+//  Created by Komolbek Ibragimov on 05/01/2022.
+//
+
+import Foundation
+
+struct Landlord {
+    private let Id: Int
+    private let name: String
+}
---
+++ Indoor-Crocodile/Models/Tenant.swift
@@ -0,0 +1,13 @@
+//
+//  Tenant.swift
+//  Indoor-Crocodile
+//
+//  Created by Komolbek Ibragimov on 05/01/2022.
+//
+
+import Foundation
+
+struct Tenant {
+    private let Id: Int
+    private let name: String
+}
